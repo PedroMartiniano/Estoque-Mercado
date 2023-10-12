b82659c3e2774c10802b0e7f4cbec729023b50fc
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-10-12 02:30:51 -0300

    sessoes tests

--- src/repositories/inMemory/SessoesRepository.ts
+++ src/repositories/inMemory/SessoesRepository.ts
@@ -1,17 +1,31 @@
 import { SessaoProps } from "../../@types/Sessoes";
 import { SessoesRepository } from "../interfaces/sessoes-interface";
+import { hash } from "bcrypt";
 
 export class InMemorySessoesRepository implements SessoesRepository {
-    private SessaoDbInMemory: SessaoProps[] = [{ id: '123', id_cliente: '321', id_func: null, email: "sessao@gmail.com", senha: '123456', status_sessao: 1 }]
+
+    private SessaoDbInMemory: SessaoProps[] = [{ id: '123', id_cliente: '321', id_func: null, email: "cliente@teste.com", senha: '123456', status_sessao: 1 }]
 
 
     async getSessaoByEmail(email: string): Promise<SessaoProps | null> {
         const sessao = this.SessaoDbInMemory.find(sessao => sessao.email === email)
 
-        if(!sessao){
+        if (!sessao) {
             return null
         }
 
         return sessao
+    }
+
+    // métodos abaixo não existentes na classe real do banco de dados
+    // foram feitos afim de otimizar testes automátizados
+    async createHashPassword() {
+        const hashPassword = await hash('123456', 4)
+
+        this.SessaoDbInMemory[0].senha = hashPassword
+    }
+
+    deactivateSessao() {
+        this.SessaoDbInMemory[0].status_sessao = 0
     }
 }
--- src/test/sessoes.test.ts
+++ src/test/sessoes.test.ts
@@ -2,22 +2,66 @@ import { describe, test, expect, beforeEach } from 'vitest'
 import { InMemorySessoesRepository } from '../repositories/inMemory/SessoesRepository'
 import { SessoesService } from '../services/SessoesService'
 import { CreateSessaoProps } from '../@types/Sessoes'
+import { AppError } from '../error/AppError'
 
 describe('should test all sessoes service components', async () => {
     let inMemorySessoesRepository: InMemorySessoesRepository
     let sessoesService: SessoesService
-    let sessaoTest1: CreateSessaoProps = {
-        email: 'email@teste.com',
+    let sessaoSuccess: CreateSessaoProps = {
+        email: 'cliente@teste.com',
         senha: '123456'
     }
+    let sessaoFailEmail: CreateSessaoProps = {
+        email: 'invalid@gmail.com',
+        senha: '123456'
+    }
+    let sessaoFailSenha: CreateSessaoProps = {
+        email: 'cliente@teste.com',
+        senha: '1234567'
+    }
 
-    beforeEach(() => {
+    beforeEach(async () => {
         inMemorySessoesRepository = new InMemorySessoesRepository
         sessoesService = new SessoesService(inMemorySessoesRepository)
+
+        await inMemorySessoesRepository.createHashPassword()
     })
 
-
     test('should get a sessao by email successfully', async () => {
-        const sessao = await sessoesService.getSessaoByEmailExecute('teste@gmail.com')
+        const sessao = await sessoesService.getSessaoByEmailExecute(sessaoSuccess.email)
+
+        expect(sessao).toBeTruthy()
+    })
+
+    test('should try get a sessao by an invalid email', async () => {
+        await expect(
+            sessoesService.getSessaoByEmailExecute(sessaoFailEmail.email)
+        ).rejects.toBeInstanceOf(AppError)
+    })
+
+    test('should do the authentication successfully', async () => {
+        const auth = await sessoesService.authUserExecute(sessaoSuccess)
+
+        expect(auth.id_cliente).toBeTypeOf('string')
+    })
+
+    test('should fail on the email authentication', async () => {
+        await expect(
+            sessoesService.authUserExecute(sessaoFailEmail)
+        ).rejects.toBeInstanceOf(AppError)
+    })
+
+    test('should fail on the password authentication', async () => {
+        await expect(
+            sessoesService.authUserExecute(sessaoFailSenha)
+        ).rejects.toBeInstanceOf(AppError)
+    })
+
+    test('should try to do the authetication with a deactivated sessao', async () => {
+        inMemorySessoesRepository.deactivateSessao()
+
+        await expect(
+            sessoesService.authUserExecute(sessaoSuccess)
+        ).rejects.toBeInstanceOf(AppError)
     })
 })
