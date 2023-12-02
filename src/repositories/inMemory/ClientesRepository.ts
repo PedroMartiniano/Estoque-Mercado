996604174c0a1ddc2be8e66ddb2eb942091fd5d5
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-12-02 00:14:14 -0300

    produtos db in memory

--- src/repositories/inMemory/ClientesRepository.ts
+++ src/repositories/inMemory/ClientesRepository.ts
@@ -3,7 +3,7 @@ import { CreateSessaoProps, SessaoProps } from "../../@types/Sessoes";
 import { ClientesRepository } from "../interfaces/clientes-interface";
 
 export class InMemoryClientesRepository implements ClientesRepository {
-    private clientDbInMemory: ClientesProps[] = []
+    private clienteDbInMemory: ClientesProps[] = []
     private clienteListInMemory: ClientesProps[] = [
         { id: '123', nome: 'first', sobrenome: 'cliente', cpf: '12345678910', status_cliente: 1 },
         { id: '321', nome: 'second', sobrenome: 'cliente', cpf: '12345678911', status_cliente: 1 }
@@ -12,14 +12,14 @@ export class InMemoryClientesRepository implements ClientesRepository {
 
     async createCliente(data: CreateClienteProps, sessao: CreateSessaoProps): Promise<ClientesProps | null> {
         const cliente: ClientesProps = {
-            id: `${this.clientDbInMemory.length}`,
+            id: `${this.clienteDbInMemory.length}`,
             nome: data.nome,
             sobrenome: data.sobrenome,
             cpf: data.cpf,
             status_cliente: 1
         }
 
-        this.clientDbInMemory.push(cliente)
+        this.clienteDbInMemory.push(cliente)
 
         const sessaoData: SessaoProps = {
             id: `${this.sessaoDbInMemory.length}`,
@@ -36,7 +36,7 @@ export class InMemoryClientesRepository implements ClientesRepository {
     }
 
     async getClienteById(id: string): Promise<ClientesProps | null> {
-        const cliente = this.clientDbInMemory.find(cliente => cliente.id === id)
+        const cliente = this.clienteDbInMemory.find(cliente => cliente.id === id)
 
         if (!cliente) {
             return null
@@ -46,7 +46,7 @@ export class InMemoryClientesRepository implements ClientesRepository {
     }
 
     async getClienteByCpf(cpf: string): Promise<ClientesProps | null> {
-        const cliente = this.clientDbInMemory.find(cliente => cliente.cpf === cpf)
+        const cliente = this.clienteDbInMemory.find(cliente => cliente.cpf === cpf)
 
         if (!cliente) {
             return null
@@ -56,32 +56,32 @@ export class InMemoryClientesRepository implements ClientesRepository {
     }
 
     async updateCliente(data: ClientesProps): Promise<ClientesProps | null> {
-        const clienteIndex = this.clientDbInMemory.findIndex(cliente => cliente.id === data.id)
+        const clienteIndex = this.clienteDbInMemory.findIndex(cliente => cliente.id === data.id)
 
         const newCliente: ClientesProps = {
-            ...this.clientDbInMemory[clienteIndex],
+            ...this.clienteDbInMemory[clienteIndex],
             nome: data.nome,
             sobrenome: data.sobrenome,
             cpf: data.cpf,
             status_cliente: data.status_cliente
         }
 
-        this.clientDbInMemory[clienteIndex] = newCliente
+        this.clienteDbInMemory[clienteIndex] = newCliente
 
-        return this.clientDbInMemory[clienteIndex]
+        return this.clienteDbInMemory[clienteIndex]
     }
 
     async deleteCliente(id: string): Promise<ClientesProps | null> {
-        const clienteIndex = this.clientDbInMemory.findIndex(cliente => cliente.id === id)
+        const clienteIndex = this.clienteDbInMemory.findIndex(cliente => cliente.id === id)
 
         const deletedCliente: ClientesProps = {
-            ...this.clientDbInMemory[clienteIndex],
+            ...this.clienteDbInMemory[clienteIndex],
             status_cliente: 0
         }
 
-        this.clientDbInMemory[clienteIndex] = deletedCliente
+        this.clienteDbInMemory[clienteIndex] = deletedCliente
 
-        return this.clientDbInMemory[clienteIndex]
+        return this.clienteDbInMemory[clienteIndex]
     }
 
     async getAllClientes(): Promise<ClientesProps[] | null> {
--- src/repositories/inMemory/ProdutosRepository.ts
+++ src/repositories/inMemory/ProdutosRepository.ts
@@ -1,5 +1,23 @@
+import { CreateProdutoProps, ProdutosProps } from "../../@types/Produtos";
 import { ProdutosRepository } from "../interfaces/produtos-interface";
 
 // export class InMemoryProdutosRepository implements ProdutosRepository {
-    
+//     private prodDbInMemory: ProdutosProps[] = []
+//     private prodListInMemory: ProdutosProps[] = [
+//         { id: '1', id_cat: '321', nome: 'ProdTeste', marca: 'MarcaTeste', imagem: 'url/image', qtde: 10, preco: 19.99, status_prod: 1 },
+//         { id: '2', id_cat: '322', nome: 'ProdTeste2', marca: 'MarcaTeste2', imagem: 'url/image2', qtde: 5, preco: 50, status_prod: 1 }
+//     ]
+
+//     async createProduto(data: CreateProdutoProps): Promise<ProdutosProps | null> {
+//         const produto = {
+//             id: `${this.prodDbInMemory.length}`,
+//             imagem: 'none',
+//             ...data,
+//             status_prod: 1
+//         }
+
+//         this.prodDbInMemory.push(produto)
+
+//         return produto
+//     }
 // }
